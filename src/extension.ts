ad23ffc589229e93baf05c651f2ad53f3adc02d8
Kaede Fujisaki <kaede@hexe.net>
2021-05-08 14:46:05 +0900

    create buttons

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,16 @@
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "build-run-debug-test-button" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('build-run-debug-test-button.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Build/Run/Debug/Test button!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(vscode.commands.registerCommand('launch-buttons.run', () => {
+    vscode.commands.executeCommand('workbench.action.debug.run');
+  }));
+  context.subscriptions.push(vscode.commands.registerCommand('launch-buttons.debug', () => {
+    vscode.commands.executeCommand('workbench.action.debug.start');
+  }));
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+
+}
