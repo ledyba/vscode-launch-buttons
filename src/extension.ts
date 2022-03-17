192c7baa91c7dbfebecc23f5f142b547041c1a1e
Kaede Fujisaki <kaede@hexe.net>
2022-03-17 18:22:20 +0900

    order tasks

--- src/extension.ts
+++ src/extension.ts
@@ -10,10 +10,31 @@ export function activate(context: vscode.ExtensionContext) {
   .then(() => {
     context.subscriptions.push(vscode.commands.registerCommand('launch-buttons.run', () => {
       vscode.tasks.fetchTasks().then((tasks) => {
+        let globalTask: vscode.Task | null = null;
+        let workspaceTask: vscode.Task | null = null;
+        let defaultTask: vscode.Task | null = null;
         for(const task of tasks) {
           if(kRunRegexp.test(task.name)){
-            return task;
+            switch(task.scope) {
+              case vscode.TaskScope.Global:
+                globalTask = task;
+                break;
+              case vscode.TaskScope.Workspace:
+                workspaceTask = task;
+                break;
+              default:
+                defaultTask = task;
+            }
           }
+        }
+        if(workspaceTask != null) {
+          return workspaceTask;
+        }
+        if(globalTask != null) {
+          return globalTask;
+        }
+        if(defaultTask != null) {
+          return defaultTask;
         }
         return null;
       }).then((runTask: vscode.Task | null) => {
